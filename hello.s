ddc721f2d2af83957bebdc3be22c2cfab99baf91
0918nobita <nobita.0918@gmail.com>
2021-01-15 17:15:20 +0900

    :sparkles: アセンブリで繰り返し処理を記述したサンプルを追加

--- hello.s
+++ hello.s
@@ -3,20 +3,26 @@
 ; ./hello
 
 bits 64
-section .text
 global _start
-
-_start:
-    mov rax, 1   ; sys_write
-    mov rdi, 1   ; stdout
-    mov rsi, msg ; address
-    mov rdx, len ; length
-    syscall
-
-    mov rax, 60  ; sys_exit
-    xor rdi, rdi ; 0
-    syscall
 
 section .data
     msg db  'hello, world', 0x0A
-    len equ $ - msg
+
+section .text
+_start:
+    mov rcx, 3    ; カウンタの初期値を設定
+.loop:
+    dec rcx       ; カウンタを減らす
+    mov rax, 1    ; sys_write
+    mov rdi, 1    ; stdout
+    mov rsi, msg  ; address
+    mov rdx, 13  ; length
+    push rcx      ; syscall が rcx を書き換えるので値をスタックに退避する
+    syscall
+    pop rcx       ; 退避していた値を戻す
+    test rcx, rcx ; rcx が 0 ならゼロフラグが立つ
+    jnz .loop     ; ゼロフラグが立っていなければジャンプする
+
+    mov rax, 60   ; sys_exit
+    xor rdi, rdi  ; 0
+    syscall
