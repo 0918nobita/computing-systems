472ff1f57c89b0fbdd9a00546aae5c4d026c7b86
0918nobita <nobita.0918@gmail.com>
2021-01-16 12:49:01 +0900

    :sparkles: レジスタの値を出力するアセンブリプログラムを追加

---
+++ asm/print-register.s
@@ -0,0 +1,33 @@
+; nasm -f elf64 print-register.s
+; ld -o stack print-register.o
+; ./print-register
+
+bits 64
+global _start
+
+section .data
+    codes db '0123456789ABCDEF'
+
+section .text
+_start:
+    mov rax, 255           ; 表示する数値 N
+    mov rdi, 1             ; ディスクリプタ (1 = stdout)
+    mov rdx, 1             ; 文字列のバイト数
+    mov rcx, 64            ; カウンタ i (64 → 0)
+.loop:
+    push rax               ; N の現在の値をスタックに退避
+    sub rcx, 4             ; i = i - 4
+    sar rax, cl            ; rax に残った N を i ビットだけ算術右シフト
+    and rax, 0xf           ; rax に rax と 0xf の論理積を格納し、16進の1桁を得る
+    lea rsi, [codes + rax] ; 文字列のアドレス
+    mov rax, 1             ; sys_write
+    push rcx               ; i をスタックに退避
+    syscall
+    pop rcx                ; i を復元
+    pop rax                ; N を復元
+    test rcx, rcx
+    jnz .loop              ; i が 0 でなければジャンプする
+
+    mov rax, 60   ; sys_exit
+    xor rdi, rdi  ; 0
+    syscall
