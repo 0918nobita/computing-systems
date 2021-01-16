f498f7eaa2e5134cd98195f1b81ed938d6277204
0918nobita <nobita.0918@gmail.com>
2021-01-16 16:27:44 +0900

    :sparkles: 文字列の長さを求めるアセンブリプログラムを追加

---
+++ asm/string-length/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern int stringLength(char* str);
+
+int main() {
+    printf("Result: %d\n", stringLength("Hello"));
+    return 0;
+}
---
+++ asm/string-length/string-length.s
@@ -0,0 +1,13 @@
+bits 64
+global stringLength
+
+section .text
+stringLength:
+    xor rax, rax
+.loop:
+    cmp byte[rdi+rax], 0
+    je .end
+    inc rax
+    jmp .loop
+.end:
+    ret
