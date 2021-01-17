6cab1677dcb3740a7948cc2c2ac85d821d41f0f4
0918nobita <nobita.0918@gmail.com>
2021-01-18 00:03:06 +0900

    :sparkles: brk システムコールを用いるアセンブリプログラムを追加した

---
+++ asm/dyn-str.s
@@ -0,0 +1,44 @@
+bits 64
+global _start
+
+section .text
+_start:
+    mov rax, 12  ; sys_brk
+    xor rdi, rdi
+    syscall
+
+    mov rdi, rax
+    add rdi, 5
+    mov rax, 12  ; sys_brk
+    syscall
+
+    mov byte[rax],   0x41
+    mov byte[rax+1], 0x42
+    mov byte[rax+2], 0x43
+    mov byte[rax+3], 0x0A
+    mov byte[rax+4], 0x00
+    mov rdi, rax
+    call printString
+
+    mov rax, 60  ; sys_exit
+    xor rdi, rdi
+    syscall
+
+printString:
+    call stringLength
+    mov rdx, rax  ; length
+    mov rax, 1    ; sys_write
+    mov rsi, rdi  ; address
+    mov rdi, 1    ; stdout
+    syscall
+    ret
+
+stringLength:
+    xor rax, rax
+.loop:
+    cmp byte[rdi+rax], 0
+    je .end
+    inc rax
+    jmp .loop
+.end:
+    ret
