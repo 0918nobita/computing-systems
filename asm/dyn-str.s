175ad23caedc68b15ff1a6558f1198eb6c8a2e7a
0918nobita <nobita.0918@gmail.com>
2021-01-26 13:26:08 +0900

    :sparkles: brk syscall で確保したヒープに文字列を書き込むサンプルを更新

--- asm/dyn-str.s
+++ asm/dyn-str.s
@@ -1,40 +1,69 @@
 bits 64
 global _start
 
+%define SYS_WRITE 1
+%define SYS_BRK 12
+%define SYS_EXIT 60
+
+section .data
+    newline db 10
+    codes db '0123456789ABCDEF'
+
 section .text
 _start:
-    mov rax, 12  ; sys_brk
+    ; 現在のブレーク値を取得
+    mov rax, SYS_BRK
     xor rdi, rdi
     syscall
 
+    ; 取得したブレーク値をデバッグ出力
+    push rax  ; base = rax
     push rax
     mov rdi, rax
+    call printInt
+    call printNewline
+
+    ; インクリメントされたブレーク値を brk システムコールで指定してヒープ領域を確保
+    pop rax
+    mov rdi, rax
     add rdi, 5
-    mov rax, 12  ; sys_brk
+    mov rax, SYS_BRK
     syscall
 
+    ; 確保した後のブレーク値をデバッグ出力
+    push rax
+    mov rdi, rax
+    call printInt
+    call printNewline
+
+    ; 確保したヒープ領域にデータを書き込む
     pop rax
     mov byte[rax],   0x41
     mov byte[rax+1], 0x42
     mov byte[rax+2], 0x43
     mov byte[rax+3], 0x0A
     mov byte[rax+4], 0x00
-    push rax
     mov rdi, rax
     call printString
 
-    mov rax, 12  ; sys_brk
-    pop rdi
+    ; もともとのブレーク値を brk システムコールで指定してヒープ領域を解放
+    mov rax, SYS_BRK
+    pop rdi      ; rdi = base
     syscall
 
-    mov rax, 60  ; sys_exit
+    ; 解放した後のブレーク値をデバッグ出力
+    mov rdi, rax
+    call printInt
+    call printNewline
+
+    mov rax, SYS_EXIT
     xor rdi, rdi
     syscall
 
 printString:
     call stringLength
     mov rdx, rax  ; length
-    mov rax, 1    ; sys_write
+    mov rax, SYS_WRITE
     mov rsi, rdi  ; address
     mov rdi, 1    ; stdout
     syscall
@@ -48,4 +77,32 @@ stringLength:
     inc rax
     jmp .loop
 .end:
+    ret
+
+printInt:
+    mov rax, rdi           ; 表示する数値 N
+    mov rdi, 1             ; ディスクリプタ (1 = stdout)
+    mov rdx, 1             ; 文字列のバイト数
+    mov rcx, 64            ; カウンタ i (64 → 0)
+.loop:
+    push rax               ; N の現在の値をスタックに退避
+    sub rcx, 4             ; i = i - 4
+    sar rax, cl            ; rax に残った N を i ビットだけ算術右シフト
+    and rax, 0xf           ; rax に rax と 0xf の論理積を格納し、16進の1桁を得る
+    lea rsi, [codes + rax] ; 文字列のアドレス
+    mov rax, SYS_WRITE
+    push rcx               ; i をスタックに退避
+    syscall
+    pop rcx                ; i を復元
+    pop rax                ; N を復元
+    test rcx, rcx
+    jnz .loop              ; i が 0 でなければジャンプする
+    ret
+
+printNewline:
+    mov rax, SYS_WRITE
+    mov rdi, 1        ; stdout
+    mov rsi, newline  ; address
+    mov rdx, 1        ; length
+    syscall
     ret
