063c55a0bd1525f7701024970c6df4d4dfd6a86f
0918nobita <nobita.0918@gmail.com>
2021-02-08 02:24:51 +0900

    :sparkles: x86エミュレータのプロジェクトを追加

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 | Path | Description |
 | --- | --- |
 | `/compiler` | BASIC language compiler written in Rust |
+| `/emulator` | x86 emulator implementation in Rust |
 | `/magazine` | Tech magazine about BASIC language compiler implementation |
 | `/samples` | Sample programs |
 | `/t2b` | Text to binary converter |
---
+++ emulator/src/main.rs
@@ -0,0 +1,62 @@
+use std::fs;
+
+#[derive(Default)]
+struct Emulator {
+    // 汎用レジスタ
+    eax: u32,
+    ecx: u32,
+    edx: u32,
+    ebx: u32,
+    esp: u32,
+    ebp: u32,
+    esi: u32,
+    edi: u32,
+    /// プログラムカウンタ
+    eip: u32,
+    /// EFLAGS レジスタ
+    eflags: u32,
+    /// メモリ (バイト列)
+    memory: Vec<u8>,
+}
+
+impl Emulator {
+    fn new(size: usize, eip: u32, esp: u32) -> Self {
+        let mut emu = Emulator::default();
+        emu.eip = eip;
+        emu.esp = esp;
+        emu.memory.resize_with(size, Default::default);
+        emu
+    }
+
+    fn dump_registers(&self) {
+        println!("EAX = {}", self.eax);
+        println!("ECX = {}", self.ecx);
+        println!("EDX = {}", self.edx);
+        println!("EBX = {}", self.ebx);
+        println!("ESP = {}", self.esp);
+        println!("EBP = {}", self.ebp);
+        println!("ESI = {}", self.esi);
+        println!("EDI = {}", self.edi);
+        println!("EIP = {}", self.eip);
+        println!("EFLAGS = {}", self.eflags);
+    }
+}
+
+fn main() {
+    let mut emu = Emulator::new(1024 * 1024 /* 1MiB */, 0x0000, 0x7c00);
+
+    load_binary(&mut emu);
+
+    emu.dump_registers();
+    println!("Memory[0..=5]: {:?}", &emu.memory[0..=5]);
+}
+
+fn load_binary(emu: &mut Emulator) {
+    if let Ok(content) = fs::read("input.bin") {
+        for (i, b) in content.iter().enumerate() {
+            emu.memory[i] = *b;
+        }
+    } else {
+        panic!("Unable to open input.bin")
+    }
+}
