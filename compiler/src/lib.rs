aabc2b20a7d77501b9feb416cc001ac85276b96a
0918nobita <nobita.0918@gmail.com>
2021-02-09 16:59:54 +0900

    :recycle: Do refactor

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -9,10 +9,11 @@ pub mod term_color;
 mod token;
 pub mod tokenizer;
 
+use clap::{app_from_crate, Arg};
 use codegen::gen_asm;
 use parser::parse;
 use sem_analysis::sem_analysis;
-use std::{fmt, path::PathBuf};
+use std::{fmt, path::PathBuf, str};
 use tokenizer::tokenize;
 
 pub struct IOInfo {
@@ -61,6 +62,43 @@ pub fn get_io_info<F: Into<PathBuf>>(filename: F) -> Result<IOInfo, String> {
     })
 }
 
+pub struct Options {
+    pub input: String,
+    pub verbose: bool,
+    pub target: Target,
+}
+
+impl Options {
+    pub fn parse() -> Self {
+        let matches = app_from_crate!()
+            .arg(Arg::new("INPUT").required(true).about("Source file"))
+            .arg(
+                Arg::new("verbose")
+                    .long("verbose")
+                    .about("Outputs verbose messages on internal operations"),
+            )
+            .arg(
+                Arg::new("target")
+                    .long("target")
+                    .takes_value(true)
+                    .possible_values(&["x86_64-linux", "x86_64-darwin"])
+                    .default_value("x86_64-linux")
+                    .about("Builds for the target triple"),
+            )
+            .get_matches();
+
+        let input = matches.value_of("INPUT").unwrap();
+        let verbose = matches.is_present("verbose");
+        let target: Target = matches.value_of("target").unwrap().parse().unwrap();
+
+        Options {
+            input: input.to_owned(),
+            verbose,
+            target,
+        }
+    }
+}
+
 pub fn compile(src: &str) -> Result<String, String> {
     let tokens = tokenize(src)?;
     let ast = parse(&tokens)?;
@@ -70,21 +108,32 @@ pub fn compile(src: &str) -> Result<String, String> {
 }
 
 pub enum Target {
-    LinuxX64,
-    MacX64,
+    X64Darwin,
+    X64Linux,
 }
 
-impl Default for Target {
-    fn default() -> Self {
-        Target::LinuxX64
+#[derive(Debug)]
+pub struct InvalidTargetError;
+
+impl str::FromStr for Target {
+    type Err = InvalidTargetError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s == "x86_64-linux" {
+            Ok(Target::X64Linux)
+        } else if s == "x86_64-darwin" {
+            Ok(Target::X64Darwin)
+        } else {
+            Err(InvalidTargetError)
+        }
     }
 }
 
-impl fmt::Debug for Target {
+impl fmt::Display for Target {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let target_name = match self {
-            Target::LinuxX64 => "Linux x86_64",
-            Target::MacX64 => "macOS x86_64",
+            Target::X64Linux => "x86_64-linux",
+            Target::X64Darwin => "x86_64-darwin",
         };
         write!(f, "{}", target_name)
     }
--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,107 +1,65 @@
 extern crate compiler;
 
-use clap::{app_from_crate, Arg};
-use compiler::{compile, get_io_info, term_color::red_bold, IOInfo, Target};
+use compiler::{compile, get_io_info, term_color::red_bold, IOInfo, Options};
 use std::{
     fs,
     process::{self, Command},
 };
 
-struct Options {
-    verbose: bool,
-    input: String,
-}
-
-impl Options {
-    fn parse() -> Self {
-        let matches = app_from_crate!()
-            .arg(Arg::new("INPUT").about("Source file").required(true))
-            .arg(
-                Arg::new("verbose")
-                    .long("verbose")
-                    .about("Output verbose messages on internal operations"),
-            )
-            .get_matches();
-
-        let input = matches.value_of("INPUT").unwrap();
-        let verbose = matches.is_present("verbose");
-
-        Options {
-            verbose,
-            input: input.to_owned(),
-        }
-    }
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let opts: Options = Options::parse();
 
-    let target = get_target().unwrap_or_default();
     if opts.verbose {
-        println!("Target: {:?}", target)
+        println!("Target: {}", opts.target)
     }
 
     let IOInfo {
         input: input_info,
         output: output_info,
-    } = get_io_info(opts.input)?;
+    } = get_io_info(opts.input).unwrap_or_else(|msg| exit_failure(&msg));
 
     let content = fs::read_to_string(input_info.src_path)
         .unwrap_or_else(|_| exit_failure("Failed to read the source file"));
 
-    let asm_output = compile(&content).unwrap_or_else(|msg| {
-        eprintln!("{}", msg);
-        process::exit(1);
+    let asm_output = compile(&content).unwrap_or_else(|msg| exit_failure(&msg));
+
+    fs::write(&output_info.asm_path, asm_output).unwrap_or_else(|err| {
+        exit_failure(&format!(
+            "{}\nError occurs while outputting assembly program",
+            err
+        ))
     });
 
-    fs::write(&output_info.asm_path, asm_output)?;
-
-    let mut nasm_cmd = Command::new("nasm");
-    nasm_cmd.args(&["-f", "elf64", output_info.asm_path.to_str().unwrap()]);
-
-    let status = nasm_cmd.status().unwrap_or_else(|err| {
-        eprintln!("{}", err);
-        eprintln!(
-            "{}",
-            red_bold("Unable to find `nasm`, perhaps install NASM and set PATH")
-        );
-        process::exit(1);
-    });
+    let status = Command::new("nasm")
+        .args(&["-f", "elf64", output_info.asm_path.to_str().unwrap()])
+        .status()
+        .unwrap_or_else(|err| {
+            exit_failure(&format!(
+                "{}\nUnable to find `nasm`, perhaps install NASM and set PATH",
+                err
+            ))
+        });
 
     if !status.success() {
         exit_failure("Error occurs while executing `nasm`");
     }
 
-    let mut ld_cmd = Command::new("ld");
-    ld_cmd.args(&[
-        "-o",
-        output_info.bin_path.to_str().unwrap(),
-        output_info.obj_path.to_str().unwrap(),
-    ]);
-
-    let status = ld_cmd.status().unwrap_or_else(|err| {
-        eprintln!("{}", err);
-        eprintln!(
-            "{}",
-            red_bold("Unable to find `ld`, perhaps install Linker and set PATH")
-        );
-        process::exit(1);
-    });
+    let status = Command::new("ld")
+        .args(&[
+            "-o",
+            output_info.bin_path.to_str().unwrap(),
+            output_info.obj_path.to_str().unwrap(),
+        ])
+        .status()
+        .unwrap_or_else(|err| {
+            exit_failure(&format!(
+                "{}\nUnable to find `ld`, perhaps install Linker and set PATH",
+                err
+            ))
+        });
 
     if !status.success() {
         exit_failure("Error occurs while executing `ld`");
-    }
-
-    Ok(())
-}
-
-fn get_target() -> Option<Target> {
-    if cfg!(all(target_arch = "x86_64", target_os = "linux")) {
-        Some(Target::LinuxX64)
-    } else if cfg!(all(target_arch = "x86_64", target_os = "macos")) {
-        Some(Target::MacX64)
-    } else {
-        None
     }
 }
 
