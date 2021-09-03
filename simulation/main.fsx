43d2362b3a8c518fe5fb5d0f2e9e3ff8e17eeff2
0918nobita <0918nobita@users.noreply.github.com>
2021-09-04 02:29:27 +0900

    Update main.fsx

--- simulation/main.fsx
+++ simulation/main.fsx
@@ -4,15 +4,17 @@ type ChipRef = ChipRef of string
 
 type PinRef = PinRef of Kind * uint8
 
+type ChipConn = ChipConn of ChipRef * PinRef seq * PinRef seq
+
 type ChipDef =
     {
         Name: string
         InPins: uint8
         OutPins: uint8
         Wires: uint8
-        Parts: ChipRef * PinRef array
+        Parts: ChipConn seq
     }
     override this.ToString() = $"[%s{this.Name}]"
 
-{ Name = "Not"; InPins = 2uy; OutPins = 2uy; Wires = 0uy; Chips = 0uy; Stmts = [| (* ... *) |] }
+{ Name = "Not"; InPins = 2uy; OutPins = 2uy; Wires = 0uy; Parts = [| (* ... *) |] }
 |> printfn "%O"
